04d1bfa50e20051a3d9bb357ee417efa0252daf4
Albert Khasanov <albert@gestiv.ru>
2018-03-13 13:27:15 +0600

    Добавлен метод класса в класс Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
 
-  # def self.name(category)
-  #   Test.joins(:categories).where("categories.title LIKE ?", category)
-  # end
+  def Test.name_of_tests(category)
+    Test.joins('LEFT JOIN categories ON categories.id=tests.category_id').where("categories.title LIKE ?", "%#{category}%")
+  end
 end
