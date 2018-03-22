eab4eda4b91ea2061937e53484ede60e76649806
Albert Khsanov <albert@gestiv.ru>
2018-03-23 00:48:20 +0600

    DZ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  def tests(level)
-    Test.joins('LEFT JOIN  tests_of_users ON tests.id = tests_of_users.test_id').where('level=?', level)
-  end
+
+  has_and_belongs_to_many :tests, join_table: "tests_of_users"
+  has_many :author_tests, class_name: "Test"
+
+  # def tests(level)
+  #   Test.joins('LEFT JOIN  tests_of_users ON tests.id = tests_of_users.test_id').where('level=?', level)
+  # end
 end
