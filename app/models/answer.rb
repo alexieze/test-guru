eeb376cab561ab7f91edcedf405806cce5bd6b41
Albert Khsanov <albert@gestiv.ru>
2018-03-24 00:37:44 +0600

    DZ path 6

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,4 @@
 class Answer < ApplicationRecord
   belongs_to :question
+  scope :correct, -> { where(correct: 1) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tests, join_table: "tests_of_users"
   has_many :author_tests, class_name: "Test"
 
-  # def tests(level)
-  #   Test.joins('LEFT JOIN  tests_of_users ON tests.id = tests_of_users.test_id').where('level=?', level)
-  # end
+  scope :tests, -> ( level ) { joins(:tests_of_users).where('level=?', level) }
+
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, with: /^[A-Za-z0-9]+$/
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
 end
